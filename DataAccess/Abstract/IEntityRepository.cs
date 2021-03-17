5efed6ccf752a665a2340e90fa73a078c04aefa7
ztaha1996@gmail.com <ztaha1996@gmail.com>
2021-03-17 19:45:21 +0300

    8.ders sonu

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Entities.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    //Generic Constraint
+    //T:class demek T türü sadece referans tür alabilir demek
+    //T:class,IEntity diyerek T için IEntity veya IEntity implement eden bir nesne olabilir demek.
+    //T : class,IEntity,new() diyerek sadece new lenebilen nesneler olabilir demiş oluyoruz bu nedenle artık T IEntity olamaz. 
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
